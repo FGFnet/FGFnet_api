a16e4f163e628c5d87f548505d51eb9d66658411
k1g99 <idmatthew9546@gmail.com>
2024-01-06 00:12:33 +0900

    feat: initial project setting

---
+++ src/main/java/com/github/fgfnet/fgfnet_api/fgfnet_api/FgfnetApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.fgfnet.fgfnet_api.fgfnet_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FgfnetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FgfnetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/fgfnet/fgfnet_api/fgfnet_api/FgfnetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fgfnet.fgfnet_api.fgfnet_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FgfnetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
